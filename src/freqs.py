b7de6dfb9ece111ace86d889994b485ac258ede2
Martin Beneš <martin.benes@stratox.com>
2024-04-06 23:54:21 +0200

    fix wrong numpy pcm array

--- src/freqs.py
+++ src/freqs.py
@@ -2,39 +2,47 @@ import serial
 import time
 import numpy as np
 import wave
+import soundfile as sf
 
 FREQ = 8000
 RATE = 9600
 
 # Define array of frequencies (example)
+
+
 class Song:
     "Song"
+
     def __init__(self, arr: np.array):
-        self.m_freq_arr = np.arange(0, np.ceil(len(arr)), dtype=np.ndarray)
-        for i, el in enumerate(arr):
-            if i % FREQ == 0:
-                np.append(self.m_freq_arr, np.array([])) # next batch
-            np.append(self.m_freq_arr[-1], el) # add freq to batch
-    
+        self.m_freq_arr = np.array_split(arr, len(arr) // FREQ)
+
     def run(self):
         "Run"
-        ser = serial.Serial('/dev/ttyUSB0', 9600)
+        ser = serial.Serial('/dev/ttyACM0', 9600)
         if not ser.is_open:
             ser.open()
         for batch in self.m_freq_arr:
             ser.write(bytes(batch))
-            time.sleep(1)
+            break
+
+
+def ConvertMusicToFreqs(filepath="files/file.vav") -> np.array:
+    data, samplerate = sf.read(filepath)
+    return data[::int(samplerate / 8000)]
+
 
 def test(file='files/speech.pcm'):
     "Test"
     with open(file, 'rb') as pcmfile:
         pcm_data = pcmfile.read()
 
-    audio_data = np.frombuffer(pcm_data, dtype=np.int16)
+    audio_data = np.frombuffer(pcm_data, dtype=np.uint8)
     audio_data = audio_data.reshape(-1, 1)
-    
-    print(audio_data)
+
+    print(f"Audio data: {audio_data.shape}")
+
     return audio_data
+
 
 tmp = Song(test())
 tmp.run()
