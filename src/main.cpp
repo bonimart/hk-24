a50a6d5601e48d5cb4decfdd3394d631e78056d4
Martin Beneš <martin.benes@stratox.com>
2024-04-06 19:54:11 +0200

    play a scale

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,71 @@
 #include <Arduino.h>
+#include <pitches.h>
 
 #define buzzerPin 3
 #define controlButtonPin A2
-#define A4 440
+#define TIMEOUT 1000
+#define NOTE_DURATION 500
+#define NOTE_PAUSE NOTE_DURATION * 1.30
 
-enum State {
-  IDLE,
-  PLAYING
-};
+void playScale();
 
-enum ButtonState {
-  PRESSED,
-  RELEASED
-};
+int readByte;
 
-void play(int, int);
-bool isPressed(int);
-
-State currentState = IDLE;
-State lastState;
-ButtonState buttonState = RELEASED;
-ButtonState lastButtonState;
-
-void setup() {
+void setup()
+{
   pinMode(buzzerPin, OUTPUT);
+  digitalWrite(buzzerPin, LOW);
   pinMode(controlButtonPin, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  lastState = currentState;
-  lastButtonState = buttonState;
-  delay(100);
-  buttonState = isPressed(controlButtonPin) ? PRESSED : RELEASED;
-  if (buttonState == RELEASED && lastButtonState == PRESSED) {
-    currentState = currentState == IDLE ? PLAYING : IDLE;
+  // playScale();
+  noTone(buzzerPin);
+  delay(10*TIMEOUT);
+}
+
+// void loop()
+// {
+//   if (Serial.available() > 0)
+//   {
+//     readByte = Serial.read();
+//     int pwmValue = map(readByte, -127, 127, 0, 255);
+//     analogWrite(buzzerPin, pwmValue);
+//     delay(1000);
+//   }
+// }
+
+  // void loop() {
+  //   // PCM data generation (replace with your PCM data)
+  //   int pcmData[] = {127, 255, 127, 0, -127}; // Example PCM data
+  //
+  //   // Output PCM data through PWM
+  //   for (int i = 0; i < sizeof(pcmData) / sizeof(pcmData[0]); i++) {
+  //     int pwmValue = map(pcmData[i], -127, 127, 0, 255);  // Map PCM data to PWM range (0-255)
+  //     analogWrite(buzzerPin, pwmValue);  // Output PWM signal
+  //     delay(1000);  // Adjust delay based on desired audio playback speed
+  //   }
+  // }
+
+void playScale()
+{
+  int scale[] = {NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_A5, NOTE_B5, NOTE_C6};
+  int scaleSize = sizeof(scale) / sizeof(scale[0]);
+  for (int i = 0; i < scaleSize; i++)
+  {
+    Serial.println(scale[i]);
+    tone(buzzerPin, scale[i], NOTE_DURATION);
+    delay(NOTE_PAUSE);
   }
-  if(currentState != lastState) {
-    if(currentState == PLAYING) {
-      tone(buzzerPin, A4);
-    }
-    else{
-      noTone(buzzerPin);
-    }
+  for (int i = scaleSize - 1; i >= 0; i--)
+  {
+    tone(buzzerPin, scale[i], NOTE_DURATION);
+    delay(NOTE_PAUSE);
   }
 }
 
-bool isPressed(int buttonPin) {
+bool isPressed(int buttonPin)
+{
   return digitalRead(buttonPin) == LOW;
 }
