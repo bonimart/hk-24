61da67ad5e4b960ca50688dadf3da3c830fda864
Martin Beneš <martin.benes@stratox.com>
2024-04-07 03:32:25 +0200

    play chord using buttons

--- src/main.cpp
+++ src/main.cpp
@@ -3,46 +3,166 @@
 
 
 #define buzzerPin 3
-#define TIMEOUT 1000
-#define NOTE_DURATION 500
+#define TIMEOUT 60
+#define NOTE_DURATION 5000
 #define NOTE_PAUSE NOTE_DURATION * 1.30
-#define FREQ 8000
+#define RATE 16192
+#define leftButton A1
+#define middleButton A2
+#define rightButton A3
+#define debounceDelay 10
+
+struct Key
+{
+    int pin;
+    int frequency;
+    bool pressed;
+    Key(int p, int f)
+    {
+        pin = p;
+        frequency = f;
+        pressed = false;
+    }
+};
 
 
+struct Tone
+{
+    int frequency;
+    uint32_t startTime;
+    int duration;
+    Tone *next;
+    Tone(int f, uint32_t s, int d)
+    {
+        frequency = f;
+        startTime = s;
+        duration = d;
+        next = NULL;
+    }
+};
+
+struct LinkedList
+{
+    unsigned length;
+    Tone *head;
+    LinkedList()
+    {
+        length = 0;
+        head = NULL;
+    }
+    void insert(int frequency, uint32_t startTime, int duration)
+    {
+        Tone *newTone = new Tone(frequency, startTime, duration);
+        if (head == NULL)
+        {
+            head = newTone;
+        }
+        else
+        {
+            newTone->next = head;
+            head = newTone;
+        }
+        length++;
+    }
+    void walk()
+    {
+        Tone *current = head;
+        Tone *prev = NULL;
+        while (current != NULL)
+        {
+            if (millis() - current->startTime > current->duration)
+            {
+                if (prev == NULL)
+                {
+                    head = current->next;
+                }
+                else
+                {
+                    prev->next = current->next;
+                }
+                current = current->next;
+                delete current;
+                length = length <= 0 ? 0 : length - 1;
+            }
+            else
+            {
+                tone(buzzerPin, current->frequency, RATE/length);
+                current = current->next;
+                prev = current;
+                delayMicroseconds(RATE/length);
+            }
+        }
+        noTone(buzzerPin);
+    }
+    ~LinkedList()
+    {
+        Tone *current = head;
+        Tone *next = NULL;
+        while (current != NULL)
+        {
+            next = current->next;
+            delete current;
+            current = next;
+        }
+    }
+
+};
+
+void playTone(int, int, float);
 void playScale();
 void scaleLoop();
-void pcmLoop();
+void handleKey(Key);
+
+LinkedList tones;
+int chord[] = {NOTE_C5, NOTE_E5, NOTE_G5}; // C major triad
+
+Key left(leftButton, NOTE_C5);
+Key middle(middleButton, NOTE_E5);
+Key right(rightButton, NOTE_G5);
+
+uint32_t lastDebounceTime = 0;
+uint32_t lastReadTime = 0;
 
 void setup()
 {
     pinMode(buzzerPin, OUTPUT);
+    pinMode(leftButton, INPUT_PULLUP);
+    pinMode(middleButton, INPUT_PULLUP);
+    pinMode(rightButton, INPUT_PULLUP);
     Serial.begin(9600);
 }
 
+
 void loop() {
-    //scaleLoop();
-    //pcmLoop();
-}
-
-char pcmData[FREQ];
-
-void pcmLoop() {
-    if (Serial.available() > 0)
+    if (millis() - lastReadTime > TIMEOUT)
     {
-        int length = Serial.readBytes(pcmData, FREQ);
-        for (int i = 0; i < length; i++)
-        {
-            int readByte = pcmData[i];
-            analogWrite(buzzerPin, readByte);
-            delayMicroseconds(10);
-        }
+        lastReadTime = millis();
+        left.pressed = digitalRead(left.pin) == LOW;
+        middle.pressed = digitalRead(middle.pin) == LOW;
+        right.pressed = digitalRead(right.pin) == LOW;
+        Serial.println(tones.length);
     }
+    handleKey(left);
+    handleKey(middle);
+    handleKey(right);
+
+    tones.walk();
 }
 
 void scaleLoop() {
     playScale();
     noTone(buzzerPin);
     delay(5000);
+}
+
+void handleKey(Key key)
+{
+    if (key.pressed && digitalRead(key.pin) == HIGH
+        && (millis() - lastDebounceTime) > debounceDelay)
+    {
+        tones.insert(key.frequency, millis(), NOTE_DURATION);
+        lastDebounceTime = millis();
+    }
 }
 
 void playScale()
